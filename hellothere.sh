ac93e392d3f91f509cba2ac4d4854b2ad215c416
lucasberenger <thegreatberenger@gmail.com>
2024-10-05 18:09:36 -0300

    first commit with some simple scripts

---
+++ hellothere.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FIRST_NAME=Lucas
+LAST_NAME=Berenger
+
+echo Hello, $FIRST_NAME $LAST_NAME
---
+++ interactiveshell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+echo What is your fist name?
+read FIRST_NAME
+
+echo What is your last name?
+read LAST_NAME
+
+echo Hello, $FIRST_NAME $LAST_NAME!
